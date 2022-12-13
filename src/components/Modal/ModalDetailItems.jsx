6891d647604fb425efc74ca79d3202f0e20fdfc9
rizrmdhn <rizrmdhn.work@gmail.com>
2022-12-13 14:23:28 +0800

    fix responsive in 1300px and 900px

--- src/components/Modal/ModalDetailItems.jsx
+++ src/components/Modal/ModalDetailItems.jsx
@@ -10,7 +10,7 @@ function ModalDetailItems({ lists, AddToCart }) {
         tabIndex="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5 ms-4" id="exampleModalLabel">
